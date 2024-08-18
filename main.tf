a4b2a047a8e68ea0ab7ca450e07a060448b494e2
Sidath <sidathm@99x.io>
2024-08-18 09:35:27 +0530

    fix permission issues

--- main.tf
+++ main.tf
@@ -24,6 +24,31 @@ resource "aws_iam_role_policy_attachment" "copilot_proxy_ecs_task_execution_role
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# Add a custom policy for CloudWatch Logs permissions
+resource "aws_iam_policy" "copilot_proxy_cloudwatch_logs_policy" {
+  name = "copilot_proxy_cloudwatch_logs_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "copilot_proxy_cloudwatch_logs_policy_attachment" {
+  role       = aws_iam_role.copilot_proxy_ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.copilot_proxy_cloudwatch_logs_policy.arn
+}
+
 # IAM policy for SQS access
 resource "aws_iam_policy" "copilot_proxy_sqs_policy" {
   name = "copilot_proxy_sqs_policy"
@@ -52,27 +77,38 @@ resource "aws_ecs_cluster" "copilot_proxy_cluster" {
 # Define the ECS task definition
 resource "aws_ecs_task_definition" "copilot_proxy_task" {
   family                   = "copilot_proxy_task"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "1024"
+  memory                   = "2048"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.copilot_proxy_ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.copilot_proxy_ecs_task_execution_role.arn
   container_definitions = jsonencode([{
     name  = "copilot_proxy_container"
     image = "${aws_ecr_repository.copilot_proxy_repo.repository_url}:latest"
     essential = true
     portMappings = [{
-      containerPort = 8080
-      hostPort      = 8080
+      name            = "8080"
+      containerPort   = 8080
+      hostPort        = 8080
+      protocol        = "tcp"
+      appProtocol     = "http"
     }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group          = "/ecs/copilot-proxy-server"
+        awslogs-region         = "us-east-1"
+        awslogs-create-group   = "true"
+        awslogs-stream-prefix  = "ecs"
+        max-buffer-size        = "25m"
+        mode                   = "non-blocking"
+      }
+    }
     environment = [
       {
         name  = "SQS_QUEUE_URL"
         value = aws_sqs_queue.copilot_events_queue.url
-      },
-      {
-        name  = "AWS_REGION"
-        value = "us-west-2"
       }
     ]
   }])
