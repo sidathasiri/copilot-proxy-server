5707e6025c0930a46e4917a3989868fd4dede16d
Sidath <sidathasiri@gmail.com>
2024-12-13 14:14:24 +0530

    remove unwanted print statement

--- src/configs.py
+++ src/configs.py
@@ -3,5 +3,3 @@ queue_url = "https://sqs.us-east-1.amazonaws.com/826406658508/copilot-events"
 # Metrics
 capturing_ghost_text_metrics = ["copilot/ghostText.shown", "copilot/ghostText.shownFromCache", "copilot/ghostText.accepted"]
 capturing_chat_metrics = ["GitHub.copilot-chat/inlineConversation.message", "GitHub.copilot-chat/inlineConversation.accept", "GitHub.copilot-chat/inlineConversation.undo"]
-
-print(queue_url)
